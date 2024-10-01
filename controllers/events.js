cc9cf2689cec5678bd4f095cae8da32363ab8a66
Camila Alm <camila.salmeidatech@hotmail.com>
2024-10-01 02:14:51 -0400

    create funcionality for the new page, POST

--- controllers/events.js
+++ controllers/events.js
@@ -16,12 +16,19 @@ router.get('/new', async (req, res) => {
     res.render('events/new.ejs');
 });
 
+//------POST-------\\
 
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);                      // Look up the user from req.session.
+        currentUser.events.push(req.body);                                                  // Push req.body (the new form data object) to the events array of the current user.
+        await currentUser.save();                                                           // Save changes to the user.
+        res.redirect(`/users/${currentUser._id}/events`);                                   // Redirect them back to the events index view.
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')                                                                   // If any errors, log them and redirect back home.
+    }
+});
 
-
-
-
-
-
-module.exports = router;                                               // Export the router so we can use it in our main server file.
+module.exports = router;                                                                    // Export the router so we can use it in our main server file.
 
